ba9634d55b967dccbfab784205bff772e30e45f3
schemr <schemryj@gmail.com>
2017-10-03 20:38:23 +0900

    Make list, create chat message and Styling UI

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database'
+
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css']
 })
 export class ChatComponent implements OnInit {
+  
+  public messages: FirebaseListObservable<any[]>
 
-  constructor() { }
+  constructor(private angularFireDatabase: AngularFireDatabase) { }
 
   ngOnInit() {
+    this.getDataBase();
   }
 
+  getDataBase() {
+    this.messages = this.angularFireDatabase.list('chat_messages');
+  }
+
+  createMessage(message) {
+    this.messages.push(message);
+  }
 }
